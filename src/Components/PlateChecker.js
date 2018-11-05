23a119b77c5cb95f210d482af7e37f5fe85acda9
Jackson Moore <jacksonmoore@tuta.io>
2018-11-05 21:54:29 +0100

    rudimentary hyphen logic

--- src/Components/PlateChecker.js
+++ src/Components/PlateChecker.js
@@ -6,12 +6,26 @@ import PlateDisplay from './PlateDisplay';
 import PlateData from './PlateData';
 
 class PlateChecker extends PureComponent {
+
+  addDashes = (plateNumber, country) => {
+    if (country === 'NL') {
+      return plateNumber.slice(0, 2) + '-' + plateNumber.slice(2, 4) + '-' + plateNumber.slice(4)
+    }
+    if (country === 'BE') {
+      return plateNumber.slice(0, 1) + '-' + plateNumber.slice(1, 4) + '-' + plateNumber.slice(4)
+    }
+    if (country === 'FR') {
+      return plateNumber.slice(0, 2) + '-' + plateNumber.slice(2, 5) + '-' + plateNumber.slice(5)
+    }
+    return plateNumber
+  }
+
   render() {
     return (
       <div className="PlateChecker">
         ZBLH89
         <PlateCheckerForm setPlate={this.props.setPlate} fetchPlateData={this.props.fetchPlateData} clearPlateData={this.props.clearPlateData} />
-        <PlateDisplay plate={this.props.plate} plateData={this.props.plateData} />
+        <PlateDisplay plate={this.props.plate} plateData={this.props.plateData} addDashes={this.addDashes} />
         <PlateData plate={this.props.plate} plateData={this.props.plateData} />
       </div>
     );
--- src/Components/PlateDisplay.js
+++ src/Components/PlateDisplay.js
@@ -4,6 +4,6 @@ import './style/PlateDisplay.css'
 export default function PlateDisplay(props) {
   if (!props.plate) return ''
   return (<div className={ `plate-display plate-display-${props.plate.land}` }>
-    {props.plate.kenteken}
+    { props.addDashes(props.plate.kenteken, props.plate.land) }
   </div>)
 }
