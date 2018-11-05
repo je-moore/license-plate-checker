7ea4855089cd5fcac23fccfd8cf4be160966bc0c
Jackson Moore <jacksonmoore@tuta.io>
2018-11-05 21:35:02 +0100

    license plate metadata styling

--- src/components/PlateData.js
+++ src/components/PlateData.js
@@ -1,23 +1,24 @@
 import * as React from 'react'
+import './style/PlateData.css'
 
 export default function PlateData(props) {
   if (!props.plateData ) return ''
   return (<div className="plate-data">
     <ul>
-      <li className="plate-datum"><span className="plate-datum-label">Aantal Cilinders</span>: { props.plateData.aantal_cilinders }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Aantal Deuren</span>: { props.plateData.aantal_deuren }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Aantal Rolstoelplaatsen</span>: { props.plateData.aantal_rolstoelplaatsen }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Aantal Wielen</span>: { props.plateData.aantal_wielen }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Aantal Zitplaatsen</span>: { props.plateData.aantal_zitplaatsen }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Datum Eerste_afgifte_nederland</span>: { props.plateData.datum_eerste_afgifte_nederland }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Datum Eerste_toelating</span>: { props.plateData.datum_eerste_toelating }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Datum Tenaamstelling</span>: { props.plateData.datum_tenaamstelling }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Eerste Kleur</span>: { props.plateData.eerste_kleur }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Inrichting</span>: { props.plateData.inrichting }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Lengte</span>: { props.plateData.lengte }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Massa Ledig Voertuig</span>: { props.plateData.massa_ledig_voertuig }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Merk</span>: { props.plateData.merk }</li>
-      <li className="plate-datum"><span className="plate-datum-label">Verzekerd</span>: { props.plateData.wam_verzekerd }</li>
+      <li className="plate-datum"><span className="plate-datum-label">Aantal Cilinders</span><span className="plate-datum-value">{ props.plateData.aantal_cilinders }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Aantal Deuren</span><span className="plate-datum-value">{ props.plateData.aantal_deuren }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Aantal Rolstoelplaatsen</span><span className="plate-datum-value">{ props.plateData.aantal_rolstoelplaatsen }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Aantal Wielen</span><span className="plate-datum-value">{ props.plateData.aantal_wielen }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Aantal Zitplaatsen</span><span className="plate-datum-value">{ props.plateData.aantal_zitplaatsen }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Datum Eerste Afgifte</span><span className="plate-datum-value">{ props.plateData.datum_eerste_afgifte_nederland }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Datum Eerste Toelating</span><span className="plate-datum-value">{ props.plateData.datum_eerste_toelating }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Datum Tenaamstelling</span><span className="plate-datum-value">{ props.plateData.datum_tenaamstelling }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Eerste Kleur</span><span className="plate-datum-value">{ props.plateData.eerste_kleur }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Inrichting</span><span className="plate-datum-value">{ props.plateData.inrichting }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Lengte</span><span className="plate-datum-value">{ props.plateData.lengte }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Massa Ledig Voertuig</span><span className="plate-datum-value">{ props.plateData.massa_ledig_voertuig }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Merk</span><span className="plate-datum-value">{ props.plateData.merk }</span></li>
+      <li className="plate-datum"><span className="plate-datum-label">Verzekerd</span><span className="plate-datum-value">{ props.plateData.wam_verzekerd }</span></li>
     </ul>
   </div>)
 }
