d1d6e93d4fc1ce8abccef55e604f72d2329a9697
Jackson Moore <jacksonmoore@tuta.io>
2018-11-06 08:48:16 +0100

    PlateData handle null and undefined values

--- src/reducers/plateData.js
+++ src/reducers/plateData.js
@@ -3,7 +3,11 @@ import { SET_PLATE_DATA } from '../actions/plates'
 export default (state = null, action = {}) => {
   switch (action.type) {
   case SET_PLATE_DATA:
-    return action.payload
+    if (action.payload === undefined) {
+      return 'undefined'
+    } else {
+      return action.payload
+    }
   default:
     return state
   }
