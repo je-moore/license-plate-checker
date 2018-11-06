99b7fbfeb81de2d7066d87ffbf5e39604ac609c2
Jackson Moore <jacksonmoore@tuta.io>
2018-11-06 08:55:15 +0100

    fix store.js redux_tools

--- src/store.js
+++ src/store.js
@@ -3,10 +3,10 @@ import reducer from './reducers'
 import ReduxThunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = compose(
-  applyMiddleware(ReduxThunk),
-  devTools
+const enhancer = composeEnhancers(
+  applyMiddleware(ReduxThunk)
 )
 
 const store = createStore(reducer, enhancer)
